f0110854a15417b912b621e7245ad5809e4168e4
Yusuf Kaymaz <kaymazyusuf88@gmail.com>
2023-04-29 11:54:43 +0300

    db

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const app = express();
 
+dotenv.config();
 
-app.listen(8800, ()=>{
-    console.log("Backend is running");
-})
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Connected to MongoDB !"))
+  .catch((err) => console.error("Failed to connect to MongoDB", err));
+app.listen(8800, () => {
+  console.log("Backend is running");
+});
